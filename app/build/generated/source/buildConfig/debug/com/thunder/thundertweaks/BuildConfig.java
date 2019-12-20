8f268a9e3144783eff938478600e8bb257aa563e
ThunderStorms21th <pinakastorm@gmail.com>
2019-12-20 12:22:23 +0100

    update gradle

--- app/build/generated/source/buildConfig/debug/com/thunder/thundertweaks/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/thunder/thundertweaks/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.thunder.thundertweaks";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1033;
-  public static final String VERSION_NAME = "1.0.3.3";
+  public static final int VERSION_CODE = 1040;
+  public static final String VERSION_NAME = "1.0.4";
 }
