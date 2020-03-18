bfc4e52cecbaae436515951c7cba9c64a066ac0e
ThunderStorms21th <pinakastorm@gmail.com>
2020-03-18 10:33:50 +0100

    update to ThunderTweaks v1.0.4.2

--- app/build/generated/source/buildConfig/debug/com/thunder/thundertweaks/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/thunder/thundertweaks/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.thunder.thundertweaks";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1040;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 1042;
+  public static final String VERSION_NAME = "1.0.4.2";
 }
