f6f2056e78ad30737b831b708064ae61d9fb9bc8
corsicanu <corsicanu22@gmail.com>
2021-06-01 16:33:05 +0300

    battery: add new battery node - this adds support for s21 and probably others devices running 5.* kernel

--- app/src/main/java/com/thunder/thundertweaks/utils/kernel/battery/Battery.java
+++ app/src/main/java/com/thunder/thundertweaks/utils/kernel/battery/Battery.java
@@ -73,7 +73,8 @@ public class Battery {
                 "/sys/devices/battery",
                 "/sys/devices/battery.30",
                 "/sys/devices/battery.55",
-                "/sys/devices/platform/battery"}
+                "/sys/devices/platform/battery",
+                "/sys/devices/platform/samsung_mobile_device/samsung_mobile_device:battery"}
                 ) {
             if (Utils.existFile(file)) {
                 BATTERY_NODE = file;
