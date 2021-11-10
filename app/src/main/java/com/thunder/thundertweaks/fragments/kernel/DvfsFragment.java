4e64801f7920a0762f56fb120bfebf6bc304c7ee
ThunderStorms21th <pinakastorm@gmail.com>
2021-11-10 12:29:48 +0100

    support for min/max freq's bus MIF DevFreq
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- app/src/main/java/com/thunder/thundertweaks/fragments/kernel/DvfsFragment.java
+++ app/src/main/java/com/thunder/thundertweaks/fragments/kernel/DvfsFragment.java
@@ -1,14 +1,24 @@
 package com.thunder.thundertweaks.fragments.kernel;
 
+import android.util.SparseArray;
+import android.text.InputType;
+
 import com.thunder.thundertweaks.R;
 import com.thunder.thundertweaks.fragments.ApplyOnBootFragment;
 import com.thunder.thundertweaks.fragments.recyclerview.RecyclerViewFragment;
 import com.thunder.thundertweaks.utils.Utils;
+import com.thunder.thundertweaks.utils.Device;
 import com.thunder.thundertweaks.utils.kernel.dvfs.Dvfs;
+import com.thunder.thundertweaks.fragments.statistics.OverallFragment;
+import com.thunder.thundertweaks.utils.kernel.bus.VoltageMif;
 import com.thunder.thundertweaks.views.recyclerview.CardView;
+import com.thunder.thundertweaks.views.recyclerview.GenericSelectView2;
 import com.thunder.thundertweaks.views.recyclerview.RecyclerViewItem;
 import com.thunder.thundertweaks.views.recyclerview.SeekBarView;
 import com.thunder.thundertweaks.views.recyclerview.SelectView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,6 +30,8 @@ import java.util.List;
 
 public class DvfsFragment extends RecyclerViewFragment {
 
+    private Dvfs mDvfs;
+
     @Override
     protected void init() {
         super.init();
@@ -30,6 +42,20 @@ public class DvfsFragment extends RecyclerViewFragment {
     @Override
     protected void addItems(List<RecyclerViewItem> items) {
 
+        if (Dvfs.hasDecisionMode()) {
+            DecisionModeInit(items);
+        }
+
+        if (Dvfs.hasThermalControl()) {
+            ThermalControlInit(items);
+        }
+
+        if (Dvfs.hasDevfreqMinFreq()) {
+            DvfsFreqInit(items);
+        }
+	}
+
+   private void DecisionModeInit(List<RecyclerViewItem> items) {
         CardView dec = new CardView(getActivity());
         dec.setTitle(getString(R.string.dvfs_decision_mode));
 
@@ -53,8 +79,9 @@ public class DvfsFragment extends RecyclerViewFragment {
         if (dec.size() > 0) {
             items.add(dec);
         }
+   }
 
-
+   private void ThermalControlInit(List<RecyclerViewItem> items) {
         CardView term = new CardView(getActivity());
         term.setTitle(getString(R.string.dvfs_thermal_control));
 
@@ -80,6 +107,52 @@ public class DvfsFragment extends RecyclerViewFragment {
         if (term.size() > 0) {
             items.add(term);
         }
-    }
+   }
 
+	String board = Device.getBoard();
+
+   private void DvfsFreqInit(List<RecyclerViewItem> items) {
+        CardView devmif = new CardView(getActivity());
+        devmif.setTitle(getString(R.string.dvfs_mif_control));
+		List<String> list = new ArrayList<>();		
+
+	if (board.equalsIgnoreCase("exynos9830") || board.equalsIgnoreCase("exynos990")) {
+        list.addAll(Arrays.asList("2730 MHz", "2535 MHz", "2288 MHz", "2028 MHz", "1716 MHz", "1539 MHz", "1352 MHz", "1014 MHz", "845 MHz", "676 MHz", "546 MHz", "421 MHz"));
+	} else if (board.equalsIgnoreCase("exynos9820") || board.equalsIgnoreCase("exynos9825")) {
+		list.addAll(Arrays.asList("2093 MHz", "1794 MHz", "1539 MHz", "1352 MHz", "1014 MHz", "845 MHz", "676 MHz", "546 MHz", "421 MHz"));
+	} else { 
+		list.addAll(Arrays.asList("not supported"));
+	}
+        SelectView mif_min = new SelectView();
+        mif_min.setTitle(getString(R.string.mif_min_freq));
+        mif_min.setSummary(getString(R.string.mif_min_freq_summary));
+        mif_min.setItems(list);
+        mif_min.setItem(Dvfs.getDevfreqMinFreq());
+        mif_min.setOnItemSelected(new SelectView.OnItemSelected() {
+            @Override
+            public void onItemSelected(SelectView mif_min, int position, String item) {
+                Dvfs.setDevfreqMinFreq(item, getActivity());
+            }
+        });
+
+        devmif.addItem(mif_min);
+
+        SelectView mif_max = new SelectView();
+        mif_max.setTitle(getString(R.string.mif_max_freq));
+        mif_max.setSummary(getString(R.string.mif_max_freq_summary));
+        mif_max.setItems(list);
+        mif_max.setItem(Dvfs.getDevfreqMaxFreq());
+        mif_max.setOnItemSelected(new SelectView.OnItemSelected() {
+            @Override
+            public void onItemSelected(SelectView mif_max, int position, String item) {
+                Dvfs.setDevfreqMaxFreq(item, getActivity());
+            }
+        });
+
+        devmif.addItem(mif_max);
+
+        if (devmif.size() > 0) {
+            items.add(devmif);
+        }
+   }
 }
--- app/src/main/java/com/thunder/thundertweaks/utils/kernel/dvfs/Dvfs.java
+++ app/src/main/java/com/thunder/thundertweaks/utils/kernel/dvfs/Dvfs.java
@@ -1,10 +1,21 @@
 package com.thunder.thundertweaks.utils.kernel.dvfs;
 
 import android.content.Context;
+import android.util.SparseArray;
 
 import com.thunder.thundertweaks.fragments.ApplyOnBootFragment;
+import com.thunder.thundertweaks.fragments.statistics.OverallFragment;
+import com.thunder.thundertweaks.utils.kernel.bus.VoltageMif;
 import com.thunder.thundertweaks.utils.Utils;
+import com.thunder.thundertweaks.utils.Device;
 import com.thunder.thundertweaks.utils.root.Control;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Morogoku on 11/04/2017.
@@ -14,6 +25,9 @@ public class Dvfs {
 
     private static final String DECISION_MODE = "/sys/devices/system/cpu/cpufreq/mp-cpufreq/cpu_dvfs_mode_control";
     private static final String THERMAL_CONTROL = "/sys/power/little_thermal_temp";
+    private static final String MIF_MAX_FREQ = "/sys/devices/platform/17000010.devfreq_mif/devfreq/17000010.devfreq_mif/max_freq";
+    private static final String MIF_MIN_FREQ = "/sys/devices/platform/17000010.devfreq_mif/devfreq/17000010.devfreq_mif/min_freq";
+	private static final String MIF_AVAILABLE_FREQ = "/sys/devices/platform/17000010.devfreq_mif/devfreq/17000010.devfreq_mif/available_frequencies";
 
     public static void setDecisionMode(String value, Context context) {
         switch (value){
@@ -44,6 +58,10 @@ public class Dvfs {
         return null;
     }
 
+    public static boolean hasDecisionMode() {
+        return Utils.existFile(DECISION_MODE);
+    }
+
     public static void setThermalControl (String value, Context context){
         run(Control.write(String.valueOf(value), THERMAL_CONTROL), THERMAL_CONTROL, context);
     }
@@ -52,12 +70,231 @@ public class Dvfs {
         return Utils.readFile(THERMAL_CONTROL);
     }
 
+    public static boolean hasThermalControl() {
+        return Utils.existFile(THERMAL_CONTROL);
+    }
+
+/*	public void setDevfreqMinFreq(int value, Context context) {
+        run(Control.write(String.valueOf(value), MIF_MIN_FREQ),
+                MIF_MIN_FREQ, context);
+    }
+
+    public static int getDevfreqMinFreq() {
+        return Utils.strToInt(Utils.readFile(MIF_MIN_FREQ));
+    }
+*/
+
+    public static void setDevfreqMinFreq(String value, Context context) {
+	String board = Device.getBoard();
+  if (board.equalsIgnoreCase("exynos9830") || board.equalsIgnoreCase("exynos990")) {
+		switch (value){
+            case "2730 MHz" :
+                run(Control.write("2730000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+            case "2535 MHz" :
+                run(Control.write("2535000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+            case "2288 MHz" :
+                run(Control.write("2288000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "2028 MHz" :
+                run(Control.write("2028000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1716 MHz" :
+                run(Control.write("1716000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1539 MHz" :
+                run(Control.write("1539000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1352 MHz" :
+                run(Control.write("1352000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1014 MHz" :
+                run(Control.write("1014000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "845 MHz" :
+                run(Control.write("845000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "676 MHz" :
+                run(Control.write("676000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "546 MHz" :
+                run(Control.write("546000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "421 MHz" :
+                run(Control.write("421000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+        }
+  } else if (board.equalsIgnoreCase("exynos9820") || board.equalsIgnoreCase("exynos9825")) {
+		switch (value){
+			case "2093 MHz" :
+                run(Control.write("2093000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1794 MHz" :
+                run(Control.write("1794000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1539 MHz" :
+                run(Control.write("1539000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1352 MHz" :
+                run(Control.write("1352000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "1014 MHz" :
+                run(Control.write("1014000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "845 MHz" :
+                run(Control.write("845000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "676 MHz" :
+                run(Control.write("676000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "546 MHz" :
+                run(Control.write("546000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+			case "421 MHz" :
+                run(Control.write("421000", MIF_MIN_FREQ), MIF_MIN_FREQ, context);
+                break;
+        }
+  } else {
+		  switch (value){
+		  case "not supported" :
+          break;
+		  }
+		}
+    }
+
+    public static String getDevfreqMinFreq() {
+        if (Utils.readFile(MIF_MIN_FREQ) != null) {
+            String value = Utils.readFile(MIF_MIN_FREQ);
+            switch (value) {
+                case "2730000":
+                    return "2730 MHz";
+                case "2535000":
+                    return "2535 MHz";
+                case "2288000":
+                    return "2288 MHz";
+                case "2028000":
+                    return "2028 MHz";
+                case "1716000":
+                    return "1716 MHz";
+                case "1539000":
+                    return "1539 MHz";
+                case "1352000":
+                    return "1352 MHz";
+                case "1014000":
+                    return "1014 MHz";
+                case "845000":
+                    return "845 MHz";
+                case "676000":
+                    return "676 MHz";
+                case "546000":
+                    return "546 MHz";
+                case "421000":
+                    return "421 MHz";
+            }
+        }
+        return null;
+    }
+
+    public static boolean hasDevfreqMinFreq() {
+        return Utils.existFile(MIF_MIN_FREQ);
+    }
+
+/*	public void setDevfreqMaxFreq(int value, Context context) {
+        run(Control.write(String.valueOf(value), MIF_MAX_FREQ),
+                MIF_MAX_FREQ, context);
+    }
+
+    public static int getDevfreqMaxFreq() {
+        return Utils.strToInt(Utils.readFile(MIF_MAX_FREQ));
+    }
+*/
+
+    public static void setDevfreqMaxFreq(String value, Context context) {
+        switch (value){
+            case "2730 MHz" :
+                run(Control.write("2730000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+            case "2535 MHz" :
+                run(Control.write("2535000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+            case "2288 MHz" :
+                run(Control.write("2288000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "2028 MHz" :
+                run(Control.write("2028000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "1716 MHz" :
+                run(Control.write("1716000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "1539 MHz" :
+                run(Control.write("1539000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "1352 MHz" :
+                run(Control.write("1352000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "1014 MHz" :
+                run(Control.write("1014000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "845 MHz" :
+                run(Control.write("845000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "676 MHz" :
+                run(Control.write("676000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "546 MHz" :
+                run(Control.write("546000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+			case "421 MHz" :
+                run(Control.write("421000", MIF_MAX_FREQ), MIF_MAX_FREQ, context);
+                break;
+        }
+    }
+
+    public static String getDevfreqMaxFreq() {
+        if (Utils.readFile(MIF_MAX_FREQ) != null) {
+            String value = Utils.readFile(MIF_MAX_FREQ);
+            switch (value) {
+                case "2730000":
+                    return "2730 MHz";
+                case "2535000":
+                    return "2535 MHz";
+                case "2288000":
+                    return "2288 MHz";
+                case "2028000":
+                    return "2028 MHz";
+                case "1716000":
+                    return "1716 MHz";
+                case "1539000":
+                    return "1539 MHz";
+                case "1352000":
+                    return "1352 MHz";
+                case "1014000":
+                    return "1014 MHz";
+                case "845000":
+                    return "845 MHz";
+                case "676000":
+                    return "676 MHz";
+                case "546000":
+                    return "546 MHz";
+                case "421000":
+                    return "421 MHz";
+            }
+        }
+        return null;
+    }
+
+    public static boolean hasDevfreqMaxFreq() {
+        return Utils.existFile(MIF_MAX_FREQ);
+    }
+	
+
     private static void run(String command, String id, Context context) {
         Control.runSetting(command, ApplyOnBootFragment.DVFS, id, context);
     }
 
-
     public static boolean supported() {
-        return Utils.existFile(DECISION_MODE) && Utils.existFile(THERMAL_CONTROL);
+        return Utils.existFile(DECISION_MODE) && Utils.existFile(THERMAL_CONTROL) || Utils.existFile(MIF_MAX_FREQ) && Utils.existFile(MIF_MIN_FREQ);
     }
+	
 }
